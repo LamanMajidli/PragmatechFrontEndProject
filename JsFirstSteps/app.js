d674136434965f80b3f461b5e8539207820a6bf9
Lyaman <95300855+LamanMajidli@users.noreply.github.com>
2022-03-16 17:24:06 +0400

    js

--- JsFirstSteps/app.js
+++ JsFirstSteps/app.js
@@ -1,3 +1,20 @@
+// task 1
+
+// a b və c ədədləri verilir. a və b ədədlərindən hansının c ədədinə daha yaxın olduğunu tapmağınız tələb olunur.
+
+// let a=prompt(" a ədədini daxil et: ");
+// let b=prompt(" b ədədini daxil et: ");
+// let c=prompt(" c ədədini daxil et: ");
+// let c_a=(Number(c)-Number(a));
+// let c_b=(Number(c)-Number(b));
+
+// if (c_a > c_b){
+//     alert(" Daxil edilən b ədədi c ədədinə daha yaxındır. ")
+// }else{
+//     alert(" Daxil edilən a ədədi c ədədinə daha yaxındır. ")
+// }
+
+
 // task 2
 
 // let a;
