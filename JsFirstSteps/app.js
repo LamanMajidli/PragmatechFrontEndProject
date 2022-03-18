62d4c47619abfdfff3efea6d5e4e1364cdd7fa45
Lyaman <95300855+LamanMajidli@users.noreply.github.com>
2022-03-18 17:51:22 +0400

    js

--- JsFirstSteps/app.js
+++ JsFirstSteps/app.js
@@ -17,18 +17,18 @@
 
 // task 2
 
-let x=5;
-console.log(typeof(x));
+// let x=5;
+// console.log(typeof(x));
 
-let a = 12345;
-let b= a.toString();
-let c;
-for (var i = b.length - 1; i >= 0; i--) {
-    c=a%10;
-    a=a-c;
-    a=a/10;
-    console.log(c);
-}
+// let a = 12345;
+// let b= a.toString();
+// let c;
+// for (var i = b.length - 1; i >= 0; i--) {
+//     c=a%10;
+//     a=a-c;
+//     a=a/10;
+//     console.log(c);
+// }
 
 // task 3
 
@@ -40,6 +40,11 @@ for (var i = b.length - 1; i >= 0; i--) {
 
 // task 4
 
-//  let azalanSira=[0, -1, -10,]
-// azalanSira.sort(function(a,b){return(b-a)})
-// document.write(azalanSira)
+
+let d = 8;
+let f =2;
+let g =6;
+let h =3;
+let arr =[d , f, g, h];
+arr.sort((a, b) => a - b);
+console.log(arr);
