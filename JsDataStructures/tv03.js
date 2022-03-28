1d3926905e6d7fddf8e5f4c5c03141561dac9938
Lyaman <95300855+LamanMajidli@users.noreply.github.com>
2022-03-28 20:37:16 +0400

    Tasks completed

---
+++ JsDataStructures/tv03.js
@@ -0,0 +1,24 @@
+let students = [{
+    ad: 'Eli',
+    soyad: 'Piriyev',
+    yas: 23
+}, {
+    ad: 'Ehmed',
+    soyad: 'Memmedov',
+    yas: 34
+}, {
+    ad: 'Sara',
+    soyad: 'Qurbanova',
+    yas: 40
+}, {
+    ad: 'Sevda',
+    soyad: 'Ezizova',
+    yas: 58
+}, {
+    ad: 'Qurban',
+    soyad: 'Sadiqzade',
+    yas: 60
+}]
+
+console.log(`Ad: ${students[0].ad}, Soyad: ${students[0].soyad}, Yaş: ${students[0].yas}`);
+console.log(`Ad: ${students[1].ad}, Soyad: ${students[1].soyad}, Yaş: ${students[1].yas}`);
