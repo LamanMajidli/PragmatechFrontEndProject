21265fbb49f073efda5308042d03b65ce3e7ca9a
Lyaman <95300855+LamanMajidli@users.noreply.github.com>
2022-04-07 16:24:14 +0400

    task13mart

--- JsFirstSteps/task.js
+++ JsFirstSteps/task.js
@@ -58,5 +58,8 @@
 //  console.log(a.toFixed());
 
 // task 6
-
+// 3 nöqtənin koordinatları verilir.Bu verilən koordinatlara görə a nöqtəsinə ən yaxın nöqtəni tapan proqram yazın
+// a_x=120,a_y=200
+// b_x=230,b_y=400
+// c_x=210,c_y=345
 
